b180e2d0da70ab33227f552b60d4f255b56e9386
Bas v.d. Wiel <bas@kompasmedia.nl>
2014-07-22 21:11:32 +0200

    Visual cleanup of keyDialog. Code is still a mess.

--- keydialog.cpp
+++ keydialog.cpp
@@ -5,11 +5,17 @@
 #include "x509.hpp"
 
 keyDialog::keyDialog(QWidget* parent) : QDialog(parent) {
-	label = new QLabel(tr("Key &size in bits:"));
+	label = new QLabel(tr("Key size in bits:"));
 	keySizeSelector = new QComboBox;
+	keySizeSelector->addItem("1024");
+	keySizeSelector->addItem("2048");
+	keySizeSelector->addItem("3072");
+	keySizeSelector->addItem("4096");
 	label->setBuddy(keySizeSelector);
 	keyResultArea = new QTextEdit;
 	keyResultArea->setFontFamily("Courier");
+	keyResultArea->setMinimumWidth(500);
+	keyResultArea->setMinimumHeight(500);
 	generateKeyButton = new QPushButton(tr("&Generate key"));
 	quitButton = new QPushButton(tr("&Quit"));
 
@@ -17,11 +23,16 @@ keyDialog::keyDialog(QWidget* parent) : QDialog(parent) {
 	connect(generateKeyButton, SIGNAL(clicked()), this, SLOT(generateKey()));
 	setWindowTitle(tr("Generate RSA private key"));
 
+	QHBoxLayout* topLeftLayout = new QHBoxLayout;
+	topLeftLayout->addWidget(label);
+	topLeftLayout->addWidget(keySizeSelector);
+
 	QVBoxLayout* leftLayout = new QVBoxLayout;
-	leftLayout->addWidget(keySizeSelector);
+	leftLayout->addLayout(topLeftLayout);
 	leftLayout->addWidget(keyResultArea);
 
 	QVBoxLayout* rightLayout = new QVBoxLayout;
+	rightLayout->addStretch();
 	rightLayout->addWidget(generateKeyButton);
 	rightLayout->addWidget(quitButton);
 
