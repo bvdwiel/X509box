14c938354e70265a85a5c0fbc439dc9986f2be2b
Bas v.d. Wiel <bas@kompasmedia.nl>
2014-07-25 20:14:24 +0200

    Fixup previous commit, which was sadly botched.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[]) {
 	QApplication app(argc, argv);
-	keyDialog* dialog = new mainDialog;
+	mainDialog* dialog = new mainDialog;
 	dialog->show();
 	return app.exec();
 }
