ad3e2104a87958d7b7f60597e045c47698c73795
Bas v.d. Wiel <bas@kompasmedia.nl>
2014-08-04 19:33:11 +0200

    csrTab contains actual controls.

--- csrtab.cpp
+++ csrtab.cpp
@@ -1,5 +1,21 @@
 #include "csrtab.hpp"
 
 csrTab::csrTab (QWidget* parent) {
-	//NOP
+    /* Initialize controls */
+	csrLabel = new QLabel(tr("Your certificate signing request (CSR):"));
+	csrField = new QTextEdit();
+	generateCsrButton = new QPushButton(tr("Generate CSR"));
+
+	/* Configure controls */
+    csrField->setFontFamily("Courier");
+	csrField->setMinimumWidth(500);
+	csrField->setMinimumHeight(450);
+	csrField->setReadOnly(true);
+
+	/* Arrange widgets into layouts */
+    QVBoxLayout* keyLayout = new QVBoxLayout;
+	keyLayout->addWidget(csrLabel);
+	keyLayout->addWidget(csrField);
+    keyLayout->addWidget(generateCsrButton);
+    setLayout(keyLayout);
 }
