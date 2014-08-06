e2afc6e8814529241374e15ad4e145e8d7a95e01
Bas v.d. Wiel <bas@kompasmedia.nl>
2014-08-06 22:59:25 +0200

    Starting point for PKCS12 header file.

--- pkcs12.hpp
+++ pkcs12.hpp
@@ -1,7 +1,26 @@
 #ifndef PKCS12_H
 #define PKCS12_H
 
+#include <gnutls/gnutls.h>
+#include <gnutls/x509.h>
+#include <gnutls/abstract.h>
+#include <gnutls/pkcs12.h>
+#include <exception>
+#include <string>
+#include <vector>
+
 class pkcs12 {
+public:
+	void addChainCertificate(std::string certificate);
+	void removeChainCertificate(std::string thumbPrint);
+	void addPrivateKey(std::string privateKey);
+	void addCertificate(std::string certificate);
+	void generateBundle();
+	void saveBundleToFile(std::string fileName);
+protected:
+	std::vector<std::string> chain;
+	std::string privateKey;
+	std::string certificate;
 };
 
 #endif /*PKCS12_H*/
