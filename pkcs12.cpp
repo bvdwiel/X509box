96ba251e471260163214756b130d8a16da1f5a5f
Bas v.d. Wiel <bas@kompasmedia.nl>
2014-08-21 16:57:37 +0200

    End-of-day commit.

--- pkcs12.cpp
+++ pkcs12.cpp
@@ -1,11 +1,21 @@
 #include "pkcs12.hpp"
 
-void pkcs12::addChainCertificate(std::string certificate) {
-	this->chain.push_back(certificate);
+pkcs12::pkcs12() {
+	gnutls_pkcs12_init(&pfx);
+	gnutls_pkcs12_bag_init(&bag);
 }
 
-void pkcs12::addPrivateKey(std::string privateKey) {
-	this->privateKey = privateKey;
+pkcs12::~pkcs12() {
+	gnutls_pkcs12_bag_deinit(bag);
+	gnutls_pkcs12_deinit(pfx);
+}
+
+void pkcs12::addChainCertificate(std::string certificate) {
+
+}
+
+void pkcs12::addPrivateKey(std::string privateKeyPEM) {
+	//NOP: Convert PEM and store result as GNUTLS datatype
 }
 
 void pkcs12::addCertificate(std::string certificate) {
