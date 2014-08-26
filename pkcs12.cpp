ae12f99abeea446772fceb52dba2567cc6557245
Bas v.d. Wiel <bas@kompasmedia.nl>
2014-08-26 14:53:04 +0200

    Allow importing PEM certificates into PKCS12 bag.

--- pkcs12.cpp
+++ pkcs12.cpp
@@ -11,7 +11,18 @@ pkcs12::~pkcs12() {
 }
 
 void pkcs12::addChainCertificate(std::string certificate) {
+	gnutls_x509_crt_t cert;
+	gnutls_x509_crt_init(&cert);
+	gnutls_datum_t pemdata;
 
+	//Load PEM certificate into GNUTLS data structures
+	pemdata.size = certificate.size();
+	pemdata.data = (unsigned char*)certificate.c_str();
+	gnutls_x509_crt_import(cert, &pemdata, GNUTLS_X509_FMT_PEM);
+
+	//Stick the cert in the bag and clean up
+	gnutls_pkcs12_bag_set_crt(bag, cert);
+	gnutls_x509_crt_deinit(cert);
 }
 
 void pkcs12::addPrivateKey(std::string privateKeyPEM) {
