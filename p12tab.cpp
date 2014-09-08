fe8933bfd4a2627626204b2baf9a294cc3d5816c
Bas v.d. Wiel <bas@kompasmedia.nl>
2014-09-08 20:58:54 +0200

    Number of intermediates in p12tab is now dynamic.

--- p12tab.cpp
+++ p12tab.cpp
@@ -1,13 +1,49 @@
 #include "p12tab.hpp"
 
 p12Tab::p12Tab(QWidget* parent) {
-    rootFileLabel = new QLabel(tr("Root CA certificate:"));
-    rootFileName = new QLineEdit;
-    rootFileBrowseButton = new QPushButton(tr("Browse..."));
-    rootFileLayout = new QHBoxLayout;
+    QVBoxLayout* windowLayout;
+    QVBoxLayout* intermediateStack;
+    intermediateStack = new QVBoxLayout;
+    windowLayout = new QVBoxLayout;
+    intermediateGroup = new QGroupBox(tr("Intermediate CA's"));
 
-    rootFileLayout->addWidget(rootFileLabel);
-    rootFileLayout->addWidget(rootFileName);
-    rootFileLayout->addWidget(rootFileBrowseButton);
-    setLayout(rootFileLayout);
+    for ( unsigned int i=0; i < 4; i++ ) {
+	intermediateStack->insertLayout(i, addCertificate());
+    }
+
+    QHBoxLayout* intermediateControlsLayout;
+    QPushButton* addIntermediateButton;
+    QPushButton* removeIntermediateButton;
+
+    intermediateControlsLayout = new QHBoxLayout;
+    addIntermediateButton = new QPushButton(tr("+"));
+    removeIntermediateButton = new QPushButton(tr("-"));
+    intermediateControlsLayout->addWidget(addIntermediateButton);
+    intermediateControlsLayout->addWidget(removeIntermediateButton);
+    intermediateControlsLayout->addStretch();
+
+    intermediateGroup->setLayout(intermediateStack);
+
+    windowLayout->addWidget(intermediateGroup);
+    windowLayout->addLayout(intermediateControlsLayout);
+    windowLayout->addStretch();
+
+    setLayout(windowLayout);
+}
+
+QHBoxLayout* p12Tab::addCertificate() {
+    QLabel* certFileLabel;
+    QLineEdit* certFileName;
+    QPushButton* certFileBrowseButton;
+    QHBoxLayout* certFileLayout;
+
+    certFileLabel = new QLabel(tr("Intermediate CA:"));
+    certFileName = new QLineEdit;
+    certFileBrowseButton = new QPushButton(tr("Browse..."));
+    certFileLayout = new QHBoxLayout;
+
+    certFileLayout->addWidget(certFileLabel);
+    certFileLayout->addWidget(certFileName);
+    certFileLayout->addWidget(certFileBrowseButton);
+    return(certFileLayout);
 }
