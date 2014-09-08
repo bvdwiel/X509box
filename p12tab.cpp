f6fc10870bde3fb0e7fce985498103a3fb70fb49
Bas v.d. Wiel <bas@kompasmedia.nl>
2014-09-08 21:34:13 +0200

    Add fields for own certificate to p12tab.

--- p12tab.cpp
+++ p12tab.cpp
@@ -2,10 +2,21 @@
 
 p12Tab::p12Tab(QWidget* parent) {
     QVBoxLayout* windowLayout;			// Layout for the whole tab
+    QGroupBox* intermediateGroup;
     QVBoxLayout* intermediateStack;		// Controls for adding intermediate CA's
     QHBoxLayout* intermediateControlsLayout;	// Add/remove controls for intermediate CA's
     QPushButton* addIntermediateButton;
     QPushButton* removeIntermediateButton;
+    QVBoxLayout* ownCertStack;
+    QHBoxLayout* ownCertFileLayout;
+    QHBoxLayout* ownKeyFileLayout;
+    QLabel* ownCertLabel;
+    QLineEdit* ownCertFileName;
+    QPushButton* ownCertFileBrowseButton;
+    QLabel* ownKeyLabel;
+    QLineEdit* ownKeyFileName;
+    QPushButton* ownKeyBrowseButton;
+    QGroupBox* ownCertGroup;
 
     intermediateStack = new QVBoxLayout;
     windowLayout = new QVBoxLayout;
@@ -26,9 +37,31 @@ p12Tab::p12Tab(QWidget* parent) {
 
     intermediateGroup->setLayout(intermediateStack);
 
+    // Put together the grouping for the user's own certificate
+    ownCertLabel = new QLabel(tr("Your certificate:"));
+    ownCertFileName = new QLineEdit;
+    ownCertFileBrowseButton = new QPushButton(tr("Browse..."));
+    ownKeyFileLayout = new QHBoxLayout;
+    ownCertFileLayout = new QHBoxLayout;
+    ownKeyLabel = new QLabel(tr("Your private key:"));
+    ownKeyFileName = new QLineEdit;
+    ownKeyBrowseButton = new QPushButton(tr("Browse..."));
+    ownCertStack = new QVBoxLayout;
+    ownCertGroup = new QGroupBox(tr("Your own X509 certificate."));
+    ownCertFileLayout->addWidget(ownCertLabel);
+    ownCertFileLayout->addWidget(ownCertFileName);
+    ownCertFileLayout->addWidget(ownCertFileBrowseButton);
+    ownKeyFileLayout->addWidget(ownKeyLabel);
+    ownKeyFileLayout->addWidget(ownKeyFileName);
+    ownKeyFileLayout->addWidget(ownKeyBrowseButton);
+    ownCertStack->addLayout(ownCertFileLayout);
+    ownCertStack->addLayout(ownKeyFileLayout);
+    ownCertGroup->setLayout(ownCertStack);
+
     // Compose the actual tab contents into a QVBoxLayout
     windowLayout->addWidget(intermediateGroup);
     windowLayout->addLayout(intermediateControlsLayout);
+    windowLayout->addWidget(ownCertGroup);
     windowLayout->addStretch();
 
     setLayout(windowLayout);
