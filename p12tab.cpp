f6ed2a4fdfadd1321fbaf4ceedc98e311d710342
Bas v.d. Wiel <bas@kompasmedia.nl>
2014-09-08 21:09:55 +0200

    Cleanup of p12tab.cpp before expanding further.

--- p12tab.cpp
+++ p12tab.cpp
@@ -1,29 +1,32 @@
 #include "p12tab.hpp"
 
 p12Tab::p12Tab(QWidget* parent) {
-    QVBoxLayout* windowLayout;
-    QVBoxLayout* intermediateStack;
+    QVBoxLayout* windowLayout;			// Layout for the whole tab
+    QVBoxLayout* intermediateStack;		// Controls for adding intermediate CA's
+    QHBoxLayout* intermediateControlsLayout;	// Add/remove controls for intermediate CA's
+    QPushButton* addIntermediateButton;
+    QPushButton* removeIntermediateButton;
+
     intermediateStack = new QVBoxLayout;
     windowLayout = new QVBoxLayout;
-    intermediateGroup = new QGroupBox(tr("Intermediate CA's"));
+    intermediateControlsLayout = new QHBoxLayout;
+    intermediateGroup = new QGroupBox(tr("Intermediate CA certificates"));
+    addIntermediateButton = new QPushButton(tr("+"));
+    removeIntermediateButton = new QPushButton(tr("-"));
 
+    // Preload tab with room for 4 intermediate CA's, should be enough for most cases.
     for ( unsigned int i=0; i < 4; i++ ) {
 	intermediateStack->insertLayout(i, addCertificate());
     }
 
-    QHBoxLayout* intermediateControlsLayout;
-    QPushButton* addIntermediateButton;
-    QPushButton* removeIntermediateButton;
-
-    intermediateControlsLayout = new QHBoxLayout;
-    addIntermediateButton = new QPushButton(tr("+"));
-    removeIntermediateButton = new QPushButton(tr("-"));
+    // Buttons to allow user to add/remove intermediate CA's
     intermediateControlsLayout->addWidget(addIntermediateButton);
     intermediateControlsLayout->addWidget(removeIntermediateButton);
     intermediateControlsLayout->addStretch();
 
     intermediateGroup->setLayout(intermediateStack);
 
+    // Compose the actual tab contents into a QVBoxLayout
     windowLayout->addWidget(intermediateGroup);
     windowLayout->addLayout(intermediateControlsLayout);
     windowLayout->addStretch();
